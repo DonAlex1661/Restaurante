1e91c2140f5d9afb78a609411752f9b2321dd4bd
DonAlex1661 <lex1661@hotmail.com>
2022-10-21 23:47:36 -0500

    Proyecto app1

--- app/src/main/java/com/lex/ensayog46/MainActivity.kt
+++ app/src/main/java/com/lex/ensayog46/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.lex.ensayog46
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun registro(btnnewuser:View){
+        val objeto1 = Intent(this,formulActivity::class.java)
+        startActivity(objeto1)
     }
 }
